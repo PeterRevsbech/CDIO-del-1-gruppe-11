d529a04a24ee9aab9d6f26d7098b1e911a2b561d
Christian <christiankyed@hotmail.com>
2019-09-30 10:33:07 +0200

    Vi har initaliseret i main metode.

--- src/Program/Player.java
+++ src/Program/Player.java
@@ -19,4 +19,7 @@ public class Player {
         return point;
     }
 
+
+
+
 }
--- src/Program/Terningespil.java
+++ src/Program/Terningespil.java
@@ -1,9 +1,43 @@
-package Program;
 
+package Program;
+import java.util.*;
 public class Terningespil {
 
-    public static void main(String[] args) {
-	// write your code here
-        // Vi tester gitßßß
+    public static void main(String[] args) {cd
+	// Vi initialiserer
+        //Opretter spillere og terninger
+        Scanner in = new Scanner(System.in);
+        die1 = new Die(6);
+        die2 = new Die(6);
+        p1 = new Player();
+        p2 = new Player();
+
+
+
+        //Opretter variable
+        int counter = 1; //Angiver om det spiller 1(ved lige) eller spiller 2(ved lige)
+
+        while(true){
+
+
+
+            break;
+        }
+
+    }
+
+    public static int getSum(int x1, int x2){
+        return x1+x2;
+    }
+
+    public static boolean getEns(int x1, int x2){
+        return x1 == x2;
+    }
+
+    public void rollDice(Player spiller){
+        die1.roll();
+        die2.roll();
+        spiller.givPoint(getSum(die1.faceValue,die2.faceValue));
+
     }
 }
