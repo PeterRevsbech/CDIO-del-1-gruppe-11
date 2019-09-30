0d07cf8127ea7fb54625a2b18b0ab33d0cac41e1
Dalmos87 <dalmozdk@gmail.com>
2019-09-30 09:39:42 +0200

    Klasser til Die og Player

--- src/Program/Die.java
+++ src/Program/Die.java
@@ -1,4 +1,32 @@
 package Program;
+import java.util.*;
 
 public class Die {
+
+    private int sideNummer = 6;
+    private int faceValue = 1;
+
+    public void Die(int x) {
+        sideNummer = x;
+    }
+
+    public void roll(){
+        Random Ran = new Random();
+        faceValue = Ran.nextInt(sideNummer) + 1;
+    }
+
+    private int getFaceValue(){
+        return faceValue;
+    }
+
+    private void setFaceValue(int y){
+        faceValue = y;
+    }
+
+    private String toString(){
+        return Integer.toString(faceValue);
+    }
+
+
+
 }
--- src/Program/Player.java
+++ src/Program/Player.java
@@ -1,4 +1,22 @@
 package Program;
 
 public class Player {
+
+    private int point = 0;
+    private String PlayerNavn = "Ukendt";
+
+    //konstruktør
+    public void Player(String navn){
+        PlayerNavn = navn;
+
+    }
+
+    public void givPoint(int x){
+        point = x + point;
+    }
+
+    public int getPoint(){
+        return point;
+    }
+
 }
